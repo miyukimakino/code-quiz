7322a3a264528ec5ba5ca3ba1574b8c7a138f23d
Miyuki Makino <>
2022-05-19 18:26:08 +0900

    [remove] 不要な変数定義の削除

--- code-quiz-makino/assets/js/index.js
+++ code-quiz-makino/assets/js/index.js
@@ -1,9 +1,7 @@
 $(function () {
   function HamburgerMenu() {
     const $navigation = $(".js-navigation");
-    const $list = $(".js-list");
     const $HamburgerMenu = $(".js-HamburgerMenu");
-    const $line = $(".js-line");
 
     function toggleMenu() {
       $navigation.toggleClass("is-show");
